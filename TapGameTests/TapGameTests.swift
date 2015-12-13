54d6b03db620197efe88387629c89a909994d48d
hotatekaoru <taymahotate@gmail.com>
2015-12-13 09:38:50 +0900

    Initial Commit

---
+++ TapGameTests/TapGameTests.swift
@@ -0,0 +1,36 @@
+//
+//  TapGameTests.swift
+//  TapGameTests
+//
+//  Created by 田山由理 on 2015/12/13.
+//  Copyright © 2015年 yuriProject. All rights reserved.
+//
+
+import XCTest
+@testable import TapGame
+
+class TapGameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
